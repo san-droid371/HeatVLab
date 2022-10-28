d00df115ed2c9eb1f7a34decfe81ac88dbd112aa
Santosh Majhee <santoshkumarmajhee3001@gmail.com>
2022-10-28 20:17:06 +0530

    Added All the required files

---
+++ script.js
@@ -0,0 +1,8 @@
+const cinflow=document.getElementById("cinflow");
+const coutflow=document.getElementById("coutflow");
+cinflow.addEventListener("click",()=>{
+    cinflow.classList.add("cinflow-active")
+})
+coutflow.addEventListener("click",()=>{
+    coutflow.classList.add("coutflow-active")
+})
